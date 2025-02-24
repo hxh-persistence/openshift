7898fa7b741075eba7ea7539ff43313d383b96ec
hxh-persistence <xiaohui.673799093@gmail.com>
2025-02-24 22:07:47 +0900

    web server

--- main.py
+++ main.py
@@ -1,23 +1,33 @@
-import time
-from datetime import datetime
+# import time
+# from datetime import datetime
 
-class MainEntry:
-    def __init__(self):
-        print('enter into __init__.')
+# class MainEntry:
+#     def __init__(self):
+#         print('enter into __init__.')
     
-    def demo_test(self):  
-        while True:
-            current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-            print(f'this is demo test from xiaohui. current_time:{current_time}')
-            time.sleep(5)
+#     def demo_test(self):  
+#         while True:
+#             current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+#             print(f'this is demo test from xiaohui. current_time:{current_time}')
+#             time.sleep(5)
 
-# business main
-def main():
-    main_instance = MainEntry()
-    main_instance.demo_test()
+# # business main
+# def main():
+#     main_instance = MainEntry()
+#     main_instance.demo_test()
+
+# if __name__ == "__main__":
+#     main()
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, this is a simple web server!"
 
 if __name__ == "__main__":
-    main()
+    app.run(host="0.0.0.0", port=5000, debug=True)
     
 
 
