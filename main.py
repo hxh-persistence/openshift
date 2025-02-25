369886418bca09e97a15a603d6db2b4ae20b5fac
hxh-persistence <xiaohui.673799093@gmail.com>
2025-02-25 16:28:06 +0900

    add

--- main.py
+++ main.py
@@ -19,15 +19,13 @@
 # if __name__ == "__main__":
 #     main()
 from flask import Flask
-import time
-from datetime import datetime
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    return "Hello, this is a simple web server!" + current_time
+    #current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    return "Hello, this is a simple web server!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8888, debug=True)
