a4156125ec588dc8a8d7e9c7e9ab0f9b1263e098
hxh-persistence <xiaohui.673799093@gmail.com>
2025-04-21 22:38:08 +0900

    txt write

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ app.secret_key = 'hxhtest'
 def login():
     return render_template('login.html', title='Login Page')
 
+#UPLOAD_FOLDER = '/usr/share/text.txt'
+UPLOAD_FOLDER = '/Users/hxh/Desktop/test/test.txt'
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
 @app.route('/submit', methods=['POST'])
 def submit():
     current_time_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
@@ -22,18 +26,31 @@ def submit():
     email = request.form.get('email')
     session['username'] = name
     session['email'] = email
-    return render_template('index.html', username=session['username'], email=session['email'])
+    try:
+        with open(UPLOAD_FOLDER, 'r', encoding='utf-8') as f:
+            all_content = f.read()
+    except FileNotFoundError:
+        all_content = ''
+    return render_template('index.html', username=session['username'], email=session['email'], all_content=all_content)
 
-UPLOAD_FOLDER = '/usr/share'
-#UPLOAD_FOLDER = '/Users/hxh/Desktop/test'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     current_time_str = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
-    file = request.files['file']
-    file.save(f"{UPLOAD_FOLDER}/{session['username']}_{current_time_str}_{file.filename}")
-    return render_template('index.html', username=session['username'], email=session['email'], message=f"{session['username']}の file upload successful")
+    # file = request.files['file']
+    # file.save(f"{UPLOAD_FOLDER}/{session['username']}_{current_time_str}_{file.filename}")
+    if request.method == 'POST':
+        new_content = request.form['content'].strip()
+        if new_content:
+            with open(UPLOAD_FOLDER, 'a', encoding='utf-8') as f:
+                f.write(new_content + '\n')
+    # 读取全部内容显示
+    try:
+        with open(UPLOAD_FOLDER, 'r', encoding='utf-8') as f:
+            all_content = f.read()
+    except FileNotFoundError:
+        all_content = ''
+    return render_template('index.html', all_content=all_content)
 
 if __name__ == "__main__":
     app.run(debug = True, host='0.0.0.0', port=8080)
